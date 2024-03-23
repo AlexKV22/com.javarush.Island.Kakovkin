7987c929b8f93de274ed16da49cc85262a91d37c
Aleksandr <sasha22.94@mail.ru>
2024-03-23 22:42:02 +0300

    new commit2

---
+++ src/Settings/Settings.java
@@ -0,0 +1,115 @@
+package Settings;
+
+public class Settings {
+    public static final int MAX_ROW_ISLAND = 100;
+    public static final int MIN_ROW_ISLAND = 0;
+    public static final int MAX_COL_ISLAND = 20;
+    public static final int MIN_COL_ISLAND = 0;
+
+    public static final int MAX_CAPACITY_PLANT = 200;
+
+
+    public static final int MAX_SPEED_BEAR = 2;
+    public static final double MAX_WEIGHT_BEAR = 500;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_BEAR = 5;
+    public static final double MAX_FOOD_NEEDED_BEAR = 80;
+    public static final int MINUS_HEALTH_ONE_STEP_BEAR = 20;
+
+
+    public static final int MAX_SPEED_EAGLE = 2;
+    public static final double MAX_WEIGHT_EAGLE = 6;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_EAGLE = 20;
+    public static final double MAX_FOOD_NEEDED_EAGLE = 1;
+    public static final int MINUS_HEALTH_ONE_STEP_EAGLE = 1;
+
+    public static final int MAX_SPEED_FOX = 2;
+    public static final double MAX_WEIGHT_FOX = 8;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_FOX = 30;
+    public static final double MAX_FOOD_NEEDED_FOX = 2;
+    public static final int MINUS_HEALTH_ONE_STEP_FOX = 2;
+
+    public static final int MAX_SPEED_SNAKE = 1;
+    public static final double MAX_WEIGHT_SNAKE = 15;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_SNAKE = 30;
+    public static final double MAX_FOOD_NEEDED_SNAKE = 3;
+    public static final int MINUS_HEALTH_ONE_STEP_SNAKE = 4;
+
+    public static final int MAX_SPEED_WOLF = 3;
+    public static final double MAX_WEIGHT_WOLF = 50;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_WOLF = 30;
+    public static final double MAX_FOOD_NEEDED_WOLF = 8;
+    public static final int MINUS_HEALTH_ONE_STEP_WOLF = 8;
+
+    public static final int MAX_SPEED_BOAR = 2;
+    public static final double MAX_WEIGHT_BOAR = 400;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_BOAR = 50;
+    public static final double MAX_FOOD_NEEDED_BOAR = 50;
+    public static final int MINUS_HEALTH_ONE_STEP_BOAR = 20;
+
+    public static final int MAX_SPEED_BUFFALO = 3;
+    public static final double MAX_WEIGHT_BUFFALO = 700;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_BUFFALO = 10;
+    public static final double MAX_FOOD_NEEDED_BUFFALO = 100;
+    public static final int MINUS_HEALTH_ONE_STEP_BUFFALO = 40;
+
+
+    public static final double MAX_WEIGHT_CATERPILLAR = 0.01;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_CATERPILLAR = 1000;
+
+    public static final int MAX_SPEED_DEAR = 4;
+    public static final double MAX_WEIGHT_DEAR = 300;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_DEAR = 20;
+    public static final double MAX_FOOD_NEEDED_DEAR = 50;
+    public static final int MINUS_HEALTH_ONE_STEP_DEAR = 30;
+
+
+    public static final int MAX_SPEED_DUCK = 4;
+    public static final double MAX_WEIGHT_DUCK = 1;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_DUCK = 200;
+    public static final double MAX_FOOD_NEEDED_DUCK = 0.15;
+    public static final double MINUS_HEALTH_ONE_STEP_DUCK = 0.05;
+
+
+    public static final int MAX_SPEED_GOAT = 3;
+    public static final double MAX_WEIGHT_GOAT = 60;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_GOAT = 140;
+    public static final double MAX_FOOD_NEEDED_GOAT = 10;
+    public static final double MINUS_HEALTH_ONE_STEP_GOAT = 5;
+
+
+    public static final int MAX_SPEED_HORSE = 4;
+    public static final double MAX_WEIGHT_HORSE = 400;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_HORSE = 20;
+    public static final double MAX_FOOD_NEEDED_HORSE = 60;
+    public static final double MINUS_HEALTH_ONE_STEP_HORSE = 30;
+
+
+    public static final int MAX_SPEED_MOUSE = 1;
+    public static final double MAX_WEIGHT_MOUSE = 0.05;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_MOUSE = 500;
+    public static final double MAX_FOOD_NEEDED_MOUSE = 0.01;
+    public static final double MINUS_HEALTH_ONE_STEP_MOUSE = 0.01;
+
+
+    public static final int MAX_SPEED_RABBIT = 2;
+    public static final double MAX_WEIGHT_RABBIT = 2;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_RABBIT = 150;
+    public static final double MAX_FOOD_NEEDED_RABBIT = 0.45;
+    public static final double MINUS_HEALTH_ONE_STEP_RABBIT = 0.5;
+
+
+    public static final int MAX_SPEED_SHEEP = 3;
+    public static final double MAX_WEIGHT_SHEEP = 70;
+    public static final int MAX_CAPACITY_IN_ONE_CELL_SHEEP = 140;
+    public static final double MAX_FOOD_NEEDED_SHEEP = 15;
+    public static final double MINUS_HEALTH_ONE_STEP_SHEEP = 5;
+
+
+
+
+
+
+
+
+
+}
